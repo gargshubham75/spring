72dcdfa8b9029f3e5b73760c11252484efbe5f27
shubhgar <shubhgar@LIN51017713.corp.capgemini.com>
2021-11-29 17:10:09 +0530

    core 4

---
+++ assignmentCore-4/src/main/java/com/practice/AssignmentCore4Application.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentCore4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentCore4Application.class, args);
+	}
+
+}
