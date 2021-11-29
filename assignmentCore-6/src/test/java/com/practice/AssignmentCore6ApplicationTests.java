069ddd529fcefb0084dea7fe03128bea65a93a07
shubhgar <shubhgar@LIN51017713.corp.capgemini.com>
2021-11-29 17:11:35 +0530

    core 6

---
+++ assignmentCore-6/src/test/java/com/practice/AssignmentCore6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentCore6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
